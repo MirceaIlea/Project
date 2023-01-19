cff466c7545f561a99525504851bcb91538d8b91
MirceaIlea <mircea.ilea95@outlook.com>
2023-01-20 01:25:27 +0200

    Add project files.

---
+++ Controllers/ProducersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.Data;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View(allProducers);
+        }
+    }
+}
---
+++ Models/Actor_Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Project.Models
+{
+    public class Actor_Movie
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public int ActorID { get; set; }
+        public Actor Actor { get; set; }
+    }
+}
